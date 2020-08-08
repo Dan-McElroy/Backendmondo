f99cc9394ff0ca7c0ae45ebedb8f635210a87b4c
Dan McElroy <daniel@mcelroys.net>
2020-08-08 20:01:23 +0200

    Added early version of model classes.

---
+++ Backendmondo.API/Models/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backendmondo.API.Models
+{
+    public class Subscription
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public User User { get; set; }
+
+        public Product Product { get; set; }
+
+        public DateTime Purchased { get; set; }
+
+        public ICollection<SubscriptionPause> Pauses { get; set; }
+    }
+}
---
+++ Backendmondo.API/Models/SubscriptionPause.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backendmondo.API.Models
+{
+    public class SubscriptionPause
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public Subscription Subscription { get; set; }
+
+        public DateTime Started { get; set; }
+
+        public DateTime Ended { get; set; }
+    }
+}
