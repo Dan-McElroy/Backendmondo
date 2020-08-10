cc88f29e8912b0c241b8177412e3cbe50c9e09d7
Dan McElroy <daniel@mcelroys.net>
2020-08-10 22:31:08 +0200

    Converted Postgres URL to connection string.

---
+++ Backendmondo.API/Helpers/PostgresHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Backendmondo.API.Helpers
+{
+    public static class PostgresHelper
+    {
+        public static string ConvertUrlToConnectionString(string url)
+        {
+            const string pattern = @"(postgres\:\/\/)(.+)\:(.*)@(.*):(\d+)\/(.+)";
+
+            var match = Regex.Match(url, pattern);
+
+            return $"User ID={match.Groups[2]};Password={match.Groups[3]};Host={match.Groups[4]};Port={match.Groups[5]};Database={match.Groups[6]};";
+        }
+    }
+}
--- Backendmondo.API/Startup.cs
+++ Backendmondo.API/Startup.cs
@@ -1,4 +1,5 @@
 using Backendmondo.API.Context;
+using Backendmondo.API.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -18,12 +19,13 @@ namespace GymondoAPI
 
         public IConfiguration Configuration { get; }
 
+        private string ConnectionString => PostgresHelper.ConvertUrlToConnectionString(Environment.GetEnvironmentVariable("DATABASE_URL"));
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(
-                options => options.UseNpgsql(
-                    Environment.GetEnvironmentVariable("DATABASE_URL"),
+                options => options.UseNpgsql(ConnectionString,
                     builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
